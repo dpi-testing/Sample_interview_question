763121dcb0fdef0bd804376d9978fa19f9e99f89
Chandhu Bhumireddy <89249579+bchandhu@users.noreply.github.com>
2024-07-09 03:17:32 +0000

    new changes

--- main.rb
+++ main.rb
@@ -1,10 +1,9 @@
 require_relative 'christmas_tree'
 require_relative 'gift'
 
-gifts = [
+gift = [
   Gift.new("Teddy Bear", "Red"),
   Gift.new("Doll", "Blue"),
-  Gift.new("Car", "Green")
 ]
 
 ChristmasTree.celebrate(5, gifts)
